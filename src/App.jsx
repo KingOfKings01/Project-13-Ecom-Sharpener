d10f0fe9074124e3eafc66bb779d1512f6e2a99e
KingOfKings01 <aasifkhan9605@gmail.com>
2024-10-17 01:30:36 +0530

    Product detail page is set by react router dynamicly.

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from './Pages/About';
 import Layout from './components/Layout/Layout';
 import Store from './Pages/Store';
 import Contact from './Pages/Contact';
+import Product from './Pages/Product';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path='/store' element={<Store />} />
+            <Route path='/store/:id' element={<Product />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<h1>Page not found 404</h1>} />
--- src/components/StoreComponents/Products.jsx
+++ src/components/StoreComponents/Products.jsx
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import productsArr from '../../assets/products';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Col, Nav } from 'react-bootstrap';
 import cartContext from '../Store/cartContext';
+import { NavLink } from 'react-router-dom';
 
 export default function Products() {
 
@@ -17,16 +18,18 @@ export default function Products() {
       <Row className="justify-content-center">
         {productsArr.map((product) => (
           <Col key={product.id} xs={12} md={6} lg={6} className="d-flex flex-column align-items-center mb-4">
-            <h4 className="text-center">{product.title}</h4>
-            <img src={product.imageUrl} alt={product.title} className="img-fluid" />
-            <div className="d-flex justify-content-between w-50  align-items-center mt-2">
-              <span>${product.price}</span>
-              <Button
-                variant="info"
-                className="text-light"
-                onClick={() => handleClick(product)}
-              >Add to Cart</Button>
-            </div>
+            <Nav.Link as={NavLink} to={"/store/" + product.id}>
+              <h4 className="text-center">{product.title}</h4>
+              <img src={product.imageUrl} alt={product.title} className="img-fluid" />
+              <div className="d-flex justify-content-between w-50  align-items-center mt-2">
+                <span>${product.price}</span>
+                <Button
+                  variant="info"
+                  className="text-light"
+                  onClick={() => handleClick(product)}
+                >Add to Cart</Button>
+              </div>
+            </Nav.Link>
           </Col>
         ))}
       </Row>
